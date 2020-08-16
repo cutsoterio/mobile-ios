337bbfd131d9eedb674e4f769801088d38b51129
Elvin <elvinkako17@gmail.com>
2020-08-16 17:51:45 +0200

    Onboarding

---
+++ Soterio/Models/OnBoardingData.swift
@@ -0,0 +1,22 @@
+//
+//  OnBoardingData.swift
+//  Soterio
+//
+//  Created by Tatenda Christopher Chinyamakobvu on 8/16/20.
+//  Copyright © 2020 Soterio. All rights reserved.
+//
+
+import Foundation
+
+struct OnBoardingData: Identifiable{
+    var id: Int
+    var image: String
+    var title: String
+    var description: String
+}
+
+var displayData: [OnBoardingData] = [
+    OnBoardingData(id: 1, image: "one", title: "Exposure notifications", description: "Get alerted when you might hae come into contact with someone who might have COVID-19."),
+    OnBoardingData(id: 2, image: "one", title: "Your data is secure", description: "We will store your data securely and safely and we do not distribute it to to third parties."),
+    OnBoardingData(id: 3, image: "one", title: "Track symptoms", description: "Update your status and symptoms in the application to help contain the spread of COVID-19.")
+]
---
+++ Soterio/Views/OnBoardingCardView.swift
@@ -0,0 +1,37 @@
+//
+//  OnBoardingCardView.swift
+//  Soterio
+//
+//  Created by Tatenda Christopher Chinyamakobvu on 8/16/20.
+//  Copyright © 2020 Soterio. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnBoardingCardView: View {
+    
+    var info: OnBoardingData
+    
+    var body: some View {
+        VStack {
+            Image(info.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 150, alignment: .center)
+            Spacer()
+                .frame(height: 64)
+            Text(info.title)
+                .font(.title)
+                .fontWeight(.medium)
+            Text(info.description)
+                .multilineTextAlignment(.center)
+                .padding()
+        }.padding()
+    }
+}
+
+struct OnBoardingCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingCardView(info: displayData[2])
+    }
+}
