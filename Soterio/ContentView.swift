5612266ab0b6b70f54d3edac9f508704d733109a
Elvin <elvinkako17@gmail.com>
2020-08-16 10:11:24 +0200

    Initial Commit

---
+++ Soterio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soterio
+//
+//  Created by Tatenda Christopher Chinyamakobvu on 8/16/20.
+//  Copyright © 2020 Soterio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
