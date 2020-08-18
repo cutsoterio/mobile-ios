330562b87e03d4169896222e3c984f25e4fdab8c
Tatenda Christopher Chinyamakobvu <tatendachris@gmail.com>
2020-08-18 15:30:32 +0200

    Added .gitignore

---
+++ Soterio/Models/Adapters/AdapterEncounterLog.swift
@@ -0,0 +1,9 @@
+//
+//  AdapterEncounterLog.swift
+//  Soterio
+//
+//  Created by Tatenda Christopher Chinyamakobvu on 5/28/1399 AP.
+//  Copyright © 1399 Soterio. All rights reserved.
+//
+
+import Foundation
